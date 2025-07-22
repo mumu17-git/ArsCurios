cbb94ac87a8688a428b4b759c3de2bc4518e6b2e
mumu17-git <moonpapa1709@gmail.com>
2025-07-22 22:47:55 +0900

    Update 1.1.1

--- src/main/java/com/mumu17/arscurios/util/ArsCuriosLivingEntity.java
+++ src/main/java/com/mumu17/arscurios/util/ArsCuriosLivingEntity.java
@@ -1,11 +1,22 @@
 package com.mumu17.arscurios.util;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ArsCuriosLivingEntity {
+public class ArsCuriosLivingEntity extends LivingEntity {
     private static final String EXTENDED_HAND_NAME = "ExtendedHand";
+
+    protected ArsCuriosLivingEntity(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
+        super(p_20966_, p_20967_);
+    }
 
     private static void saveExtendedHandToTag(CompoundTag tag, @NotNull String hand) {
         tag.putString(EXTENDED_HAND_NAME, hand);
@@ -34,5 +45,32 @@ public class ArsCuriosLivingEntity {
             return ExtendedHand.MAIN_HAND;
         }
         return ExtendedHand.getSlotByName(handName);
+    }
+
+    @Override
+    public @NotNull Iterable<ItemStack> getArmorSlots() {
+        return ((Player) (LivingEntity) this).getArmorSlots();
+    }
+
+    @Override
+    public @NotNull ItemStack getItemBySlot(@Nullable EquipmentSlot p_21127_) {
+        if (p_21127_ == null) {
+            return ArsCuriosInventoryHelper.getCuriosInventoryItem(this, getPlayerExtendedHand(this).getSlotName());
+        }
+        return ((Player) (LivingEntity) this).getItemBySlot(p_21127_);
+    }
+
+    @Override
+    public void setItemSlot(@Nullable EquipmentSlot p_21036_, @NotNull ItemStack p_21037_) {
+    if (p_21036_ == null) {
+            ArsCuriosInventoryHelper.setCuriosInventoryItem(this, getPlayerExtendedHand(this).getSlotName(), p_21037_);
+            return;
+        }
+        ((Player) (LivingEntity) this).setItemSlot(p_21036_, p_21037_);
+    }
+
+    @Override
+    public @NotNull HumanoidArm getMainArm() {
+        return ((Player) (LivingEntity) this).getMainArm();
     }
 }
